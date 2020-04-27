55172134d1d9258636ed00afcf419f037cc364ff
MagistrAVSH <art046@hotmail.com>
2020-04-27 21:22:42 +0300

    New physics flags

--- Iterum.Core/Physics/Objects/PhysicsObjectFlags.cs
+++ Iterum.Core/Physics/Objects/PhysicsObjectFlags.cs
@@ -8,6 +8,8 @@ namespace Iterum.Physics
         None = 0,
         Trigger = 1 << 0,
         DisableGravity = 1 << 1,
-        Kinematic = 1 << 2
+        Kinematic = 1 << 2,
+        CCD = 1 << 3,
+        Retain = 1 << 4,
     }
 }
