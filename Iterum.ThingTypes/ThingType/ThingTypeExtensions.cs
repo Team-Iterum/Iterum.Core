6eae3699648c0126a915184a2aeb503470e0aa59
MagistrAVSH <art046@hotmail.com>
2020-06-08 12:13:48 +0300

    ThingTypes extension

--- Iterum.ThingTypes/ThingType/ThingTypeExtensions.cs
+++ Iterum.ThingTypes/ThingType/ThingTypeExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Iterum.DataBlocks;
 
@@ -5,11 +6,6 @@ namespace Iterum.ThingTypes
 {
     public static class ThingTypeExtensions
     {
-        public static T GetData<T>(this IThingType tt) where T : class, IDataBlock
-        {
-            return ((ThingType)tt).DataBlocks.FirstOrDefault(e => e is T) as T;
-        }
-        
         public static string GetAttr(this IThingType tt, string attr)
         {
             if (tt.Attrs == null) return null;
@@ -18,7 +14,7 @@ namespace Iterum.ThingTypes
         }
 
         #region Float Attr Accessors
-        
+
         public static float GetFloat(this IThingType tt, string attr)
         {
             return float.TryParse(tt.GetAttr(attr), out float result) ? result : 0;
@@ -27,41 +23,53 @@ namespace Iterum.ThingTypes
         public static float[] GetFloat2(this IThingType tt, string attr)
         {
             var str = tt.GetAttr(attr).Split(' ');
-            return new[] {float.Parse(str[0]), float.Parse(str[1])};
+            return new[] { float.Parse(str[0]), float.Parse(str[1]) };
         }
+
         public static float[] GetFloat3(this IThingType tt, string attr)
         {
             var str = tt.GetAttr(attr).Split(' ');
-            return new[] {float.Parse(str[0]), float.Parse(str[1]), float.Parse(str[2])};
+            return new[] { float.Parse(str[0]), float.Parse(str[1]), float.Parse(str[2]) };
         }
-        
+
         #endregion
-        
-        
+
+
         #region Int Attrs Accessors
-        
+
         public static int GetInt(this IThingType tt, string attr)
         {
             return int.TryParse(tt.GetAttr(attr), out int result) ? result : 0;
         }
+
         public static int[] GetInt2(this IThingType tt, string attr)
         {
             var str = tt.GetAttr(attr).Split(' ');
-            return new[] {int.Parse(str[0]), int.Parse(str[1])};
+            return new[] { int.Parse(str[0]), int.Parse(str[1]) };
         }
+
         public static int[] GetInt3(this IThingType tt, string attr)
         {
             var str = tt.GetAttr(attr).Split(' ');
-            return new[] {int.Parse(str[0]), int.Parse(str[1]), int.Parse(str[2])};
+            return new[] { int.Parse(str[0]), int.Parse(str[1]), int.Parse(str[2]) };
         }
+
         #endregion
-        
-        
+
+        public static T GetEnum<T>(this IThingType tt, string attr) where T : struct, Enum
+        {
+            return Enum.Parse<T>(tt.GetAttr(attr), true);
+        }
+
+        public static T GetData<T>(this IThingType tt) where T : class, IDataBlock
+        {
+            return ((ThingType) tt).DataBlocks.FirstOrDefault(e => e is T) as T;
+        }
+
+
         public static bool HasFlag(this IThingType tt, string flag)
         {
             return tt.Flags != null && tt.Flags.Contains(flag);
         }
-
-        
     }
 }
