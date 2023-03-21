eadf610a0cdf4606324a19921961925a2b3adda8
Magistr_AVSH Cats'A <art046@hotmail.com>
2023-03-21 23:03:30 +0100

    serializers

--- Iterum.ThingTypes/ThingType/BinaryThingTypeSerializer.cs
+++ Iterum.ThingTypes/ThingType/BinaryThingTypeSerializer.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Iterum.ThingTypes
@@ -29,6 +31,58 @@ namespace Iterum.ThingTypes
             var binaryFormatter = new BinaryFormatter();
             binaryFormatter.Serialize(File.OpenWrite(fileName), tt);
         }
+        
+        public void SerializeAll(string fileName, ThingTypeStore store)
+        {
+            fileName = Path.ChangeExtension(fileName, FileExtension);
+            
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            var fs = File.OpenWrite(fileName);
+            var binaryFormatter = new BinaryFormatter();
+            foreach (var (_, thingType) in store.ThingTypes)
+            {
+                var memoryStream = new MemoryStream();
+                binaryFormatter.Serialize(memoryStream, thingType);
+                var buffer = memoryStream.ToArray();
+                var length = BitConverter.GetBytes(buffer.Length);
+                fs.Write(length);
+                fs.Write(memoryStream.ToArray());
+            }
+            
+            fs.Flush();
+            fs.Close();
+        }
+        
+        public ThingTypeStore DeserializeAll(string fileName)
+        {
+            var fs = File.OpenRead(fileName);
+            var binaryFormatter = new BinaryFormatter();
+
+            var thingTypes = new List<ThingType>();
+            while (fs.CanRead)
+            {
+                Span<byte> lengthBuffer = new byte[4];
+
+                var _ = fs.Read(lengthBuffer);
+
+                var length = BitConverter.ToInt32(lengthBuffer);
+
+                var ms = new MemoryStream(new byte[length]);
+                var thingType = (ThingType)binaryFormatter.Deserialize(ms);
+
+                thingTypes.Add(thingType);
+            }
+
+            fs.Flush();
+            fs.Close();
+
+            return new ThingTypeStore(thingTypes);
+        }
+
         
         
     }
