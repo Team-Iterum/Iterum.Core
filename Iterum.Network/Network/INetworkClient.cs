867f14803ddc109e54c43564bd57934d6e4a0265
MagistrAVSH <art046@hotmail.com>
2024-05-09 14:41:03 +0200

    INetworkClient interface

---
+++ Iterum.Network/Network/INetworkClient.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Iterum.Network;
+
+public interface INetworkClient
+{
+    event Action<NetworkMessage> Received;
+    event Action<ConnectionData> Connected;
+    event Action<ConnectionData> Disconnected;
+        
+    void Stop();
+    void Connect(string host, int port);
+    void Disconnect();
+        
+    void Send<T>(T packet) where T : struct, ISerializablePacketSegment;
+    void Send(byte[] packet);
+}
