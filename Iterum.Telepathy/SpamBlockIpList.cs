2c98c59c2081971d19d2bbe484edce4169b90d98
MagistrAVSH <art046@hotmail.com>
2026-08-18 17:40:23 +0400

    Telepathy update

--- Iterum.Telepathy/SpamBlockIpList.cs
+++ Iterum.Telepathy/SpamBlockIpList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Iterum.Network
@@ -13,6 +14,8 @@ namespace Iterum.Network
 
         public static async void CheckIncoming(string address)
         {
+            if (IsLoopback(address)) return;
+
             var result = await OnCheck(address);
             if(result)
             {
@@ -21,11 +24,22 @@ namespace Iterum.Network
         }
         public static async void Check(string address)
         {
+            if (IsLoopback(address)) return;
+
             var result = await OnAdd(address);
             if(result)
             {
                 Addresses.Add(address);
             }
+        }
+
+        // addresses arrive as "::ffff:127.0.0.1" on dual-stack listeners
+        public static bool IsLoopback(string address)
+        {
+            if (!IPAddress.TryParse(address, out var ip)) return false;
+
+            if (ip.IsIPv4MappedToIPv6) ip = ip.MapToIPv4();
+            return IPAddress.IsLoopback(ip);
         }
 
         public static Func<string, Task<bool>> OnAdd = e => Task.FromResult(true);
